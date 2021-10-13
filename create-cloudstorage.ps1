49fbf7c83711a6596e343f4de3a09a27fce15509
NikaT8868 <71405380+NikaT8868@users.noreply.github.com>
2021-10-13 08:40:54 -0500

    Add files via upload

---
+++ create-cloudstorage.ps1
@@ -0,0 +1,14 @@
+#How to create a Storage Account
+ 
+New-AzStorageAccount -ResourceGroupName "UALR-Nika-Tavasoly-rg" `
+  -Name "nikatavasolysa" `
+  -Location eastus `
+  -SkuName "Standard_LRS" `
+  -Kind StorageV2
+#Create a File Share inside the Storage account, run each command one at a time after creating storage account
+ 
+$StorageAccountName = get-azstorageaccount -StorageAccountName "nikatavasolysa" -ResourceGroupName "UALR-Nika-Tavasoly-rg"
+ 
+New-AzRmStorageShare -StorageAccount $StorageAccountName -Name "nikatavasolysa"
+ 
+After running the commands, Create a Cloud Shell instance as follows:
