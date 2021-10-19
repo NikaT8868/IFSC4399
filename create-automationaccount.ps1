fe6197d64aafdce26f4cbb32c4286ef84412b197
NikaT8868 <71405380+NikaT8868@users.noreply.github.com>
2021-10-19 13:41:28 -0500

    Add files via upload

---
+++ create-automationaccount.ps1
@@ -0,0 +1,3 @@
+
+#Create Azure Automation Account in your Azure resource groups.
+New-AzureRmAutomationAccount -Name "automation-tavasoly" -Location "East US" -ResourceGroupName "UALR-Nika-Tavasoly-rg"
