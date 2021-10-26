17223cb7f2522019053b3b1f2bd4a6bc467b92c1
NikaT8868 <71405380+NikaT8868@users.noreply.github.com>
2021-10-26 06:31:12 -0500

    Add files via upload

---
+++ create-virtual machine.ps1
@@ -0,0 +1,28 @@
+
+#Create Virtual Machine
+$VMLocalAdminUser = "nika"
+$VMLocalAdminSecurePassword = ConvertTo-SecureString "Raha&Rose" -AsPlainText -Force
+$LocationName = "eastus"
+$ResourceGroupName = "UALR-Nika-Tavasoly-rg"
+$ComputerName = "Myclientvmtava"
+$VMName = "MyVM-tavasoly"
+$VMSize = "Standard_B2ms"
+
+$NetworkName = "ualr4399-tavasoly-VNet1"
+$NICName = "MyNIC-tavasoly"
+$SubnetName = "default"
+$SubnetAddressPrefix = "10.0.0.0/24"
+$VnetAddressPrefix = "10.0.0.0/16"
+
+$SingleSubnet = New-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
+$Vnet = New-AzVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $LocationName -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
+$NIC = New-AzNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $Vnet.Subnets[0].Id
+
+$Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
+
+$VirtualMachine = New-AzVMConfig -VMName $VMName -VMSize $VMSize
+$VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
+$VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2012-R2-Datacenter' -Version latest
+
+New-AzVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $VirtualMachine -Verbose
